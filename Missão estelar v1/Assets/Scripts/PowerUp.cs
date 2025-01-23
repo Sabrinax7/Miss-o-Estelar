dabf8d3e9d4de2142f488d1ee689bfde19156e8d
Sabrinax7 <145375466+Sabrinax7@users.noreply.github.com>
2025-01-23 14:17:23 -0300

    confianoprogresso

--- Missão estelar v1/Assets/Scripts/PowerUp.cs
+++ Missão estelar v1/Assets/Scripts/PowerUp.cs
@@ -7,28 +7,33 @@ public class PowerUp : MonoBehaviour
 {
 
     public int Score;
+    private SpriteRenderer sr;
+    private CircleCollider2D circle;
+
+    
     
     void Start()
     {
+        sr = GetComponent<SpriteRenderer>();
+        circle = GetComponent<CircleCollider2D>();
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  
 
     void OnTriggerEnter2D(Collider2D collider)
     {
 
             if(collider.gameObject.tag == "Player")
             {
+                
+                  sr.enabled = false;
+                  circle.enabled = false;
                   GameController.instance.totalScore += Score;
                   GameController.instance.UpdateScoreText();
 
 
-                 Destroy (gameObject);
+                 Destroy (gameObject, 2f);
             }
     }
 }
